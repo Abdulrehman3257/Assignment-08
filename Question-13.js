cbbb9de07a4eac60f3b5b19b387664b6a8af602b
decentzidi72 <decentzidi722gmail.com>
2023-02-14 09:14:02 -0800

    Modify Q13

--- Question-13.js
+++ Question-13.js
@@ -4,9 +4,35 @@
 // Basic Salary <= 20000 : HRA = 25%, DA = 90%
 // Basic Salary > 20000 : HRA = 30%, DA = 95%
 
-let basicSalary = 5000;
+let basicSalary = 1000;
+let HRA
+let DA
+let GS
 if (basicSalary <= 10000) {
-    console.log((basicSalary /20)*100);
+    HRA = ((20 / 100) * basicSalary);
+    DA = ((80 / 100) * basicSalary);
+    GS = (HRA + DA + basicSalary);
+    console.log("Basic Salary: " + basicSalary);
+    console.log("HRA: " + HRA);
+    console.log("DA: " + DA);
+    console.log("Gross Salary: " + GS);
+
+} else if (basicSalary <= 20000) {
+    HRA = ((25 / 100) * basicSalary);
+    DA = ((90 / 100) * basicSalary);
+    GS = (HRA + DA + basicSalary);
+    console.log("Basic Salary: " + basicSalary);
+    console.log("HRA: " + HRA);
+    console.log("DA: " + DA);
+    console.log("Gross Salary: " + GS);
+
 } else {
-    console.log(basicSalary);
+    HRA = ((30 / 100) * basicSalary);
+    DA = ((95 / 100) * basicSalary);
+    GS = (HRA + DA + basicSalary);
+    console.log("Basic Salary: " + basicSalary);
+    console.log("HRA: " + HRA);
+    console.log("DA: " + DA);
+    console.log("Gross Salary: " + GS);
+
 }
